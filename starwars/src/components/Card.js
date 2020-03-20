4e91b9a58d66949bf1a488b0f6e8bdbd61ab5a6f
Rachele Edwards <dot.com1472@gmail.com>
2020-03-20 10:55:46 -0600

    everything is showing--going to look at mvp before playing around with the homeworlds again

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -3,14 +3,14 @@ import React from "react"
 const Card = props => {
     return (
         <div className="card-container">
-            <h3>Name of Character {props.name}</h3>
+            <h3>{props.name}</h3>
             <p>Gender: {props.gender}</p>
             <p>Height: {props.height}</p>
-            <p>Hair Color: </p>
-            <p>Eye Color: </p>
-            <p>Birth Year: </p>
-            <p>HomeWorld: Testing</p>
-            <p>Films Appeared in: Testing</p>
+            <p>Hair Color: {props.hair}</p>
+            <p>Eye Color: {props.eye}</p>
+            <p>Birth Year: {props.dob}</p>
+            <p>HomeWorld: Testing {props.homeworld}</p>
+            <p>Films Appeared in: {props.films}</p>
         </div>
     )
 }
--- starwars/src/components/ParentCard.js
+++ starwars/src/components/ParentCard.js
@@ -5,8 +5,9 @@ import axios from "axios"
 const ParentCard = () => {
     //set up our states
     const [profile, setProfile] = useState([])
+    const [planet, setPlanet] = useState("1")
 
-    //useEffect
+    //useEffect for profile
     useEffect(()=>{
         axios.get(`https://swapi.co/api/people/`)
         .then(response => {
@@ -18,10 +19,23 @@ const ParentCard = () => {
         })
     }, []) //end of useEffect
 
+    //useEffect for planet
+    // useEffect(()=>{
+    //     axios.get(`https://swapi.co/api/planets/${planet}/`)
+    //     .then(response=> {
+    //         console.log('PLANET RESPONSE', response)
+    //         setPlanet(response.data.name)
+    //     })
+    //     .catch(error=>{
+    //         console.log('PLANET ERROR', error)
+    //     })
+
+    // }, [planet])
+
     return (
         <div>
             {profile.map((item, index)=> {
-               return <Card key={index} name={item.name} gender ={item.gender} height={item.height} />
+               return <Card key={index} name={item.name} gender ={item.gender} height={item.height} hair={item.hair_color} eye={item.eye_color} dob={item.birth_year} homeworld={item.homeworld} films={item.films.length}/>
             })}
         </div>
     )
